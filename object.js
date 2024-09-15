6637005bc34e6ce0ddcb4cca3d7a4dc28b1aea49
Tanha <nusrattanha897@gmail.com>
2024-09-15 16:37:10 +0600

    disadvantages of procudural programming

---
+++ object.js
@@ -0,0 +1,4 @@
+// question: does javascript really have classes?
+/**
+ * Yes, javascript does have classes. This feature was introduced with the ECMAScript 2015 specification. Javascript is primarily a prototype-based language, and its classes are primarily syntactic sugar over existing prototype-based inheritance mechanisms. In other words, it provides a more familiar syntax for developers coming from class-based languages like C++, Java, but under the hood it works somewhat differently.
+ */ 
---
+++ stack learner/oop-stack-learner.js
@@ -0,0 +1,23 @@
+// before oop
+/**
+ * info: in c programming, using struct
+ * String name = "Hello"
+ * String email = "hello@gmail.com"
+ * String password = "!2sdf@34"
+ * String userName = "hello"
+ * 
+ * Struct user1 = {data}
+ * Struct user2 = {data}
+ * 
+ * login(Struct user1)
+ * Login(Struct user2)
+ * 
+ * info: Disadvantages of procedural programming
+ * Procedural programming is good, but there is some serious issue we face when we create software for real world. Mapping real world objects, data to programming seems really hard when it comes to procedural programming like C. 
+ *  1. code is not reusable
+ *  2. large code base is hard to manage
+ *  3. difficult to tracing and fixing bugs
+ *  4. data is exposed to whole program
+ *  5. operation's priority is higher than data
+ *  6. difficult to relate with real world data
+ */
